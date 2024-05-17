1d84a95a00e53290c068e7a1ef62ec4192a52980
Matt <matheus.a.barreto12@gmail.com>
2024-05-16 23:30:44 -0300

    Primeiro commit do projeto Camada de apresentação

---
+++ src/io/github/mattalvb/clientes/Main.java
@@ -0,0 +1,12 @@
+package io.github.mattalvb.clientes;
+
+
+import io.github.mattalvb.clientes.dominio.apresentacao.TelaCadastro;
+
+public class Main {
+    public static void main(String[] args) {
+        TelaCadastro telaCadastro = new TelaCadastro();
+
+        telaCadastro.setVisible(true);
+    }
+}
